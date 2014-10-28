7dc2c5bd437f700e8ae166853212433583bdb641
hmessar <ali.hmessar@gmail.com>
2014-10-29 00:12:24 +0100

    envoi

---
+++ Navigateur/application/controllers/FlickrController.php
@@ -0,0 +1,24 @@
+<?php
+
+class FlickrController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+	
+	public function flickrAction() {
+	
+	echo "Navigateur Spatio temporel";
+	
+	}
+
+
+}
+
