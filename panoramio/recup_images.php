0bb80e480967e26f5d2abd8504394f95be8911e0
AchrafLansari <achraflansari@gmail.com>
2014-11-14 14:44:16 +0100

    panoramio fes

---
+++ panoramio/recup_images.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo file_get_contents("http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=20&minx=-180&miny=-90&maxx=180&maxy=90&size=medium&mapfilter=true");
+
+
+?>
+
