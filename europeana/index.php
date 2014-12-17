b53aef1683b964f39c32a487852f7b21f954a109
AchrafLansari <achraflansari@gmail.com>
2014-12-17 13:48:33 +0100

    date corriger

--- europeana/index.php
+++ europeana/index.php
@@ -11,8 +11,8 @@
         
         $key = 'wRRkGVyyG';
         $query= "Paris";
-        $date_d = "2000";
-        $date_f = "2014";
+        $date_d = "1863";
+        $date_f = "1900";
         $path = "http://www.europeana.eu/api/v2/search.json?wskey=".$key."&query=".$query;
         $path2 = "http://europeana.eu/api/v2/search.json?wskey=".$key."&query=".$query."&qf=YEAR%3A%5B".$date_d."+TO+".$date_f."%5D&start=1&rows=24&profile=standard";
         
@@ -24,10 +24,19 @@
         echo '<br>'.count($parsed_json['items']).'<br>';
         
         foreach ($parsed_json['items'] as $items) {
+        
+            
+        $url_image = explode('?uri=', $items['edmPreview'][0]); 
+        $url_image = explode('&size',$url_image[1]); 
+       
             
         echo $items['id'].'===>'.$items['dcCreator'][0].'===>'.$items['title'][0].'<br>';
         echo '<img src='.$items['edmPreview'][0].'>';    
+        //echo '<img src='.urldecode($url_image[0]).'>';
+        
         }
+       echo urldecode("http://europeanastatic.eu/api/image?uri=http%3A%2F%2Ffotothek.slub-dresden.de%2Fthumbs%2Fdf_ge_0015125.jpg");
+         
 ?>        
        
     </body>
