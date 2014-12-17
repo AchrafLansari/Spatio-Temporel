1240b6a46837fc9be60e4ce8cf78b9f0fb32fec0
AchrafLansari <achraflansari@gmail.com>
2014-12-17 13:07:59 +0100

    ajax requete europeana a rajouter

---
+++ europeana/ajax.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+   
+   $('#valider').click(function(){
+       
+       
+       var requete = $('#requete').val();
+       var dataString = 'requete='+requete;
+       
+       
+       
+       $.ajax({
+                        type: "POST",
+                        url: "select.php",
+                        data: dataString,
+		    	dataType: 'json',                //data format      
+                        success: function(data)          //on recieve of reply
+                        {   
+                           
+                            
+                         if(data['correction'] != undefined){
+                            
+                         $('#correction').append(data['correction']); 
+                         $('#output').html(" "); 
+                        
+                         }else {
+                             
+                            data.forEach(function(entry) {
+                             
+                             $('#correction').html(" "); 
+                            
+                            var titre = entry['titre'];
+                            var url = entry['url'];           
+                            var poids = entry['poids']; 
+      
+                            $('#output').html("<b> mot: </b>"+requete+"<b>titre: </b>"+titre+"<b> url: </b>"+url+"<b> poids: </b>"+poids);     //Set output element html
+                         
+                    
+                            });   
+                         }
+                        } 
+    });
+    
+   });
+   
+   
+   
+   $('#requete-corpus').bind("enterKey",function(e){
+        
+        
+    });
+        
+   
+    $('#requete-corpus').keyup(function(e){
+        if(e.keyCode == 13)
+        {
+        $(this).trigger("enterKey");
+        }   
+    });
+   
+    
+});
+
+
