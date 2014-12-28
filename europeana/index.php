e2b749c0680110b86615b8755dd788dc69f6cdc2
AchrafLansari <achraflansari@gmail.com>
2014-12-28 20:59:44 +0100

    différent type de search

--- europeana/index.php
+++ europeana/index.php
@@ -16,7 +16,27 @@
         $path = "http://www.europeana.eu/api/v2/search.json?wskey=".$key."&query=".$query."&rows=94";
         $path2 = "http://europeana.eu/api/v2/search.json?wskey=".$key."&query=".$query."&qf=YEAR%3A%5B".$date_d."+TO+".$date_f."%5D&start=1&rows=96&profile=standard";
         
-        $json = file_get_contents($path);
+        //search of person  des resultats sans geoposition ni year
+        $path3 = "http://www.europeana.eu/api/v2/search.json?wskey=".$key."&query=who:Leonardo+da+Vinci";
+        
+        //http://www.europeana.eu/api/v2/search.json?wskey=xxxx&query=mona+AND+lisa a executer au query
+        
+        // search of place or many with OR like the example : a corriger
+        $path4 ="http://www.europeana.eu/api/v2/search.json?wskey=".$key."&query=where:(Paris+OR+London)";
+        
+        // search with latitude + longitude 
+        $path5 = "http://www.europeana.eu/api/v2/search.json?wskey=xxxx&query=pl_wgs84_pos_lat:[45+TO+47]+AND+pl_wgs84_pos_long:[7+TO+8]";
+        
+        // search with concatenation
+        $path6 = "http://www.europeana.eu/api/v2/search.json?wskey=xxxx&query=Westminster&qf=where:London";
+        
+        // search with image type only 
+        $path7 = "http://www.europeana.eu/api/v2/search.json?wskey=xxxx&query=Paris&qf=TYPE:IMAGE";
+        
+        //search with range date 
+        $path8 ="http://www.europeana.eu/api/v2/search.json?wskey=xxxx&query=timestamp_created:[2013-11-01T00:00:0.000Z+TO+2013-12-01T00:00:00.000Z]";
+        
+        $json = file_get_contents($path4);
         $parsed_json = json_decode($json,true);
         echo '<br>';
         var_dump($parsed_json);
