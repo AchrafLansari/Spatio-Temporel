7bb6d40593c0a24806d6c2b104b74da07abbef0d
hmessar <ali.hmessar@gmail.com>
2014-12-31 01:21:20 +0100

    random ca marche bien mais recupiration de la dernier caractere pour tester j arrive pas

--- wikipedia Api/index_pays.php
+++ wikipedia Api/index_pays.php
@@ -2,9 +2,30 @@
 	$texte = file_get_contents("pays.txt");
 	$tab_mots = tokenisation("\n", $texte); 
         
-	
-	print_tab($tab_mots);
-	
+        $rand_keys = array();
+        $band_keys1 = array();
+       for($i = 0 ; $i < count($tab_mots); $i++) {
+           
+             // jaffiche bien les payes par random  
+              $band_keys1 = "_" . $tab_mots[mt_rand (0, count($tab_mots)-1)] . "<br>";
+              
+              // mais ici quand je recupire le derniere caractere j'arrive 
+               $rand_afficher = substr($band_keys1,strlen($band_keys1)-2, 1);
+               
+              //ici je teste  la derniere caractere 
+              
+           // echo $band_keys1 . " ->" . strlen(count($band_keys1)) . "<br>";
+          /*   if(strcmp($rand_afficher ,"e")==0){
+                 echo "en_" .  $band_keys1 . "<br>";
+             }
+             else {
+                 echo "au_" . $band_keys1 . "<br>";
+             }         
+           * 
+           */
+        }
+  
+         afficher($band_keys1);
 	function tokenisation($separateurs, $texte) 
 	{
 		$arrayElements = array();
@@ -22,9 +43,11 @@
 	//Afficher un tableau
 	function print_tab($tab_mots) 
 	{
+            $i=0;
 		foreach($tab_mots as $valeur) 
 		{
-			echo  $valeur, "<br>"; 
+			echo "$i => " . $valeur. strlen($valeur). "<br>";
+                        $i++;
 		}
 	}
 
