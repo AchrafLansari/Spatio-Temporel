f8c7fe4c129f32c02414eea655579de577a36acb
hmessar <ali.hmessar@gmail.com>
2015-01-18 10:39:18 +0100

    quel que modif des payes

--- wikipedia Api/index.php
+++ wikipedia Api/index.php
@@ -1,149 +1,94 @@
 <?php
-
 include 'globals.php';
-
-
 function tokenization ($text,$delimiteurs,$nb_carac,$state)
-        {
-            $arrayElements = array();
-            global $tab_mots_vides;
-            $words = strtok($text,$delimiteurs);
-            
-            
-             while ($words !== false){
-                    if($state == 0){
-                        $words = trim(str_replace('&nbsp','',$words));
-                        $cpt = strlen($words);
-                     if($cpt >= $nb_carac){
-                         if (!array_key_exists($words,array_flip($tab_mots_vides)))
-                             {
-                                $arrayElements[]=$words;
-                             }
-                         }
-                     
-                    }else {
-                        $arrayElements[]=$words;
-                    }
-                     $words = strtok($delimiteurs);
-                 
-             }
-             return $arrayElements;
-        }
-
-
-$api_url = 'http://fr.wikipedia.org/w/api.php';
-
-
-$wiki = new Wikimate($api_url);
-
-
-
-
-
-
-#$wiki->setDebugMode(TRUE);
-$texte = file_get_contents("Data/pays.txt");
-	$tab_mots = tokenization($texte , "\n",0,1); 
-        
-        $rand_keys = array();
-        $band_keys1 = array();
-         $rand_afficher = array();
-           
-              $band_keys1 =  $tab_mots[mt_rand (0, count($tab_mots)-1)];
-               
-              //var_dump(rtrim($band_keys1));
-               $rand_afficher = substr(rtrim($band_keys1) , -1);
-             //  var_dump($rand_afficher);
-//var_dump(substr($band_keys1 ,0,strlen($band_keys1)));
-            if($rand_afficher =='e'){
-               // Combodge,Mexique,Mozambique, ZaÔre Zimbabwe
-                if( (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Mexique' ) 
-                     or (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Combodge' )
-                     or (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Mozambique' )
-                     or  (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='ZaÔre Zimbabwe' )
-                  )  
+    {
+        $arrayElements = array();
+        global $tab_mots_vides;
+        $words = strtok($text,$delimiteurs);          
+        while ($words !== false){
+            if($state == 0){
+                $words = trim(str_replace('&nbsp','',$words));
+                $cpt = strlen($words);
+            if($cpt >= $nb_carac){
+            if (!array_key_exists($words,array_flip($tab_mots_vides)))
                 {
-                    $paye =  "au_"  .(substr($band_keys1 ,0,strlen($band_keys1))) ;
-                    
+                $arrayElements[]=$words;
                 }
-                else
-                    $paye = "en_"  .substr($band_keys1 , 0,strlen($band_keys1));
-             }
-             else {
-                 if( (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Cuba')){
-                 $paye = "‡_"  .substr($band_keys1 ,0,strlen($band_keys1));
-                 }else if( (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Kiribati') || (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Ile Christmas')  ){
+            }
+            }
+            else{
+                $arrayElements[]=$words;
+                }
+                $words = strtok($delimiteurs);
+        }
+        return $arrayElements;
+        }
+$api_url = 'http://fr.wikipedia.org/w/api.php';
+$wiki = new Wikimate($api_url);
+$texte = file_get_contents("Data/pays.txt");
+    $tab_mots = tokenization($texte , "\n",0,1); 
+    $rand_keys = array();
+    $band_keys1 = array();
+    $rand_afficher = array();
+    $band_keys1 =  $tab_mots[mt_rand (0, count($tab_mots)-1)];
+    $rand_afficher = substr(rtrim($band_keys1) , -1);
+    
+        if($rand_afficher =='e'){
+        $pays_test = substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)));
+        if(($pays_test =='Mexique' ) 
+                or ($pays_test=='Combodge' )
+                or ($pays_test=='Mozambique' )
+                or  ($pays_test=='Za√Øre Zimbabwe') 
+                
+                        
+              )  
+        {
+            $paye =  "au_"  .(substr($band_keys1 ,0,strlen($band_keys1))) ;
+        }
+         elseif ($pays_test =='Guadeloupe')
+               $paye = "en_" . 'guadalupe';
+         
+        else 
+           $paye = "en_"  .substr($band_keys1 , 0,strlen($band_keys1));
+      
+        }
+        elseif ($pays_test =='Iles_Heard_et_McDonald')
+               $paye = "au_" . 'iles heart et mcdonald';
+        elseif ($pays_test =='Tok√©laou')
+               $paye = "au_" . 'Tok√©lou';
+        else {
+        if( (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Cuba')){
+                $paye = "ÔøΩ_"  .substr($band_keys1 ,0,strlen($band_keys1));
+            }
+        else if( (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Kiribati') || (substr(rtrim($band_keys1) ,0,strlen(rtrim($band_keys1)))=='Ile Christmas')  ){
                  $paye = "aux_"  .substr($band_keys1 ,0,strlen($band_keys1));
-                 }
-                 else {
+                }
+        else {
                  $paye = "au_"  .substr($band_keys1 ,0,strlen($band_keys1));
              }
-             }
-
-            
-            $pays = str_replace (" ", "_", $paye);
-            
-             
-            
+             } 
+             $pays = str_replace (" ", "_", $paye);
             $url_final = 'Liste_du_patrimoine_mondial_'.$pays;
-            $url_final = rtrim($url_final);
-            
-            var_dump($url_final);echo'<br>';
-            
+            $url_final = rtrim($url_final);            
+            var_dump($url_final);echo'<br>';            
             $page = $wiki->getPage($url_final);
-
-// check if the page exists or not
-if (!$page->exists() ) {
+    if (!$page->exists() ) {
 	echo "Page doesn't exist.\n";
-
-} else {
-        /*
-	// get the page title
-	echo "Title: ".$page->getTitle()."<br>";
-	// get the number of sections on the page
-	echo "Number of sections: ".$page->getNumSections()."<br>";
-	// get an array of where each section starts and its length
-	echo "Section offsets:".print_r($page->getSectionOffsets(), true)."<br>";
-        
-        //Statistiques de la liste du patrimoine
-        //echo "<br>".$page->getSection('Statistiques');
-        
-        // la liste indicative
-        //echo "<br>".utf8_decode($page->getSection('Liste indicative'));
-        
-        //liste patrimoine mondiale
-        echo "<br>".utf8_decode($page->getSection('Patrimoine mondial'))."<br>";
-        
-        //echo 'page : <br>';
-        //echo $page->getText(true);
-        */
+       } 
+    else {
         $section = utf8_decode($page->getSection('Patrimoine mondial'));
-        $tab = tokenization($section,"{}",0,1);
-        
-        
-        // commence par 1 pour enlever le titre inutile 
+        $tab = tokenization($section,"{}",0,1);        
         for($i=1;$i<count($tab);$i++){
-           // echo $tab[$i].'<br>';
-            
-            $coord = explode("name=", $tab[$i]);
-            
+        $coord = explode("name=", $tab[$i]);            
             if (isset($coord[1])) { 
                echo $coord[1].'<br>';
             }
             $lien = explode("http", $tab[$i]);
-            
             if (isset($lien[1])) { 
-               //echo "http".substr($lien[1],0,strlen($lien[1])-7).'<br>';
                 $lien_final = explode("]",$lien[1]);
-                echo '<a href="http'.$lien_final[0].'" target="_blank"> --></a> <br>';
-                
+                echo '<a href="http'.$lien_final[0].'" target="_blank"> --></a> <br>'; 
             }
-            
-            
         }
-  
-        
-        // note : on peut pas recuperer les images : l'url recupÈrer n'est pas descriptive de l'url ou se trouve l'image
 }
 
 
