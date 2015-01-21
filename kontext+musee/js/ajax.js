c8fbca7ce216d7c32676b286eb235687facec101
hmessar <ali.hmessar@gmail.com>
2015-01-21 16:23:29 +0100

    commit conext + musee

---
+++ kontext+musee/js/ajax.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    
+     
+    $('.bubblingG').css('display','none');
+    
+   
+   $('#submitButton').click(function(){
+       
+       
+       var requete = $('#s').val();
+       var rows = $('#rows').val();
+       var dataString="query='"+requete+"'&rows="+rows;
+       
+       $('.bubblingG').css('display','block');
+       
+       $.ajax({
+                        url: "../europeana/requete/simple.php",
+                        type: "POST",
+                        dataType: 'json',
+                        data: { query : requete,
+                                rows : rows
+                              },
+                        success: function(data)          //on recieve of reply
+                        {   
+                           //console.log('Response received : '+data["completeness"]);
+                           $('.bubblingG').css('display','none'); 
+                           data.forEach(function(entry) {
+                                
+                                $('#output').append(entry["completeness"]+"==>"+entry["image"]+"<br>");     //Set output element html
+                          });   
+                         
+                        },error : function(xhr, status){
+                            console.log(status);
+                        }
+    });
+    
+   });
+   
+   
+   
+   /*$('#requete-corpus').bind("enterKey",function(e){
+        
+        
+    });
+        
+   
+    $('#requete-corpus').keyup(function(e){
+        if(e.keyCode == 13)
+        {
+        $(this).trigger("enterKey");
+        }   
+    });*/
+   
+    
+});
+
+
