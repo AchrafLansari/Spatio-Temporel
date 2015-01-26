8f6164c3acef04f31e828186d0c76ef3a15a13a2
AchrafLansari <achraflansari@gmail.com>
2015-01-26 18:29:11 +0100

    fav icon

--- kontext+musee/index.php
+++ kontext+musee/index.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" href="css/demo.css">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <link rel="icon" type="image/png" href="icones/12316-babasse-safari.png" />
 
 
                 <link rel="stylesheet" >
